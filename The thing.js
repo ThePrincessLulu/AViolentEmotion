56943f547557c0dd813b80cb66e6b437b6961b5d
ThePrincessLulu <iloveartsunnyleila@gmail.com>
2017-01-25 14:08:42 -0600

    Update The thing.js

--- The thing.js
+++ The thing.js
@@ -5,10 +5,17 @@ const bot = new Discord.Client();
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.username}!`);
 });
-bot.on('message', msg => {
-    if (msg.content === 'ping') {
-    msg.reply('Pong!');
-}
+var responseObject = {
+ "hey" : "Hey.",
+ "please" : "no thank you",
+ "Ryan" : "you're not cool",
+  "ping" : "Pong!"'
+  "I won" : "That's what you think"  
+};
+bot.on('message', (message) => {
+  if(responseObject[message.content]) {
+    message.channel.sendMessage(responseObject[message.content]);
+  }
 });
 bot.on('message', msg => {
     if (msg.content === 'please') {
